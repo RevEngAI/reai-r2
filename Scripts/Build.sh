9a3de643cedfb3870fdc207ada3a6cf52c7defc4
Siddharth Mishra <admin@brightprogrammer.in>
2025-04-25 14:07:12 +0530

    Fix For  On MacOSX

---
+++ Scripts/Build.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+InstallPath="/usr/local"
+echo "Dependencies will be installed at prefix $InstallPath"
+
+cd /tmp
+git clone https://github.com/revengai/reai-r2
+git clone https://github.com/revengai/creait
+git clone https://github.com/brightprogrammer/tomlc99
+git clone https://github.com/DaveGamble/cJSON
+
+# Build and install cjson
+cmake -S "/tmp/cjson" \
+    -B "/tmp/cjson/Build" \
+    -D ENABLE_CUSTOM_COMPILER_FLAGS=OFF \
+    -D CMAKE_PREFIX_PATH="$InstallPath" \
+    -D CMAKE_INSTALL_PREFIX="$InstallPath" \
+    -D CMAKE_INSTALL_RPATH="$InstallPath/lib" \
+    -DCMAKE_POLICY_VERSION_MINIMUM="3.5"
+cmake --build "/tmp/cjson/Build" --config Release
+sudo cmake --install "/tmp/cjson/Build" --prefix "$InstallPath" --config Release
+
+# Build and install tomlc99 
+cmake -S "/tmp/tomlc99" \
+    -B "/tmp/tomlc99/Build" \
+    -D CMAKE_PREFIX_PATH="$InstallPath" \
+    -D CMAKE_INSTALL_PREFIX="$InstallPath" \
+    -D CMAKE_INSTALL_RPATH="$InstallPath/lib" \
+    -DCMAKE_POLICY_VERSION_MINIMUM="3.5"
+cmake --build "/tmp/tomlc99/Build" --config Release
+sudo cmake --install "/tmp/tomlc99/Build" --prefix "$InstallPath" --config Release
+
+# Build and install creait
+cmake -S "/tmp/creait" \
+    -B "/tmp/creait/Build" \
+    -D CMAKE_PREFIX_PATH="$InstallPath" \
+    -D CMAKE_INSTALL_PREFIX="$InstallPath" \
+    -D CMAKE_INSTALL_RPATH="$InstallPath/lib" \
+    -D CMAKE_LIBRARY_PATH="$InstallPath/lib" \
+    -D BUILD_SHARED_LIBS=OFF \
+    -DCMAKE_POLICY_VERSION_MINIMUM="3.5"
+cmake --build "/tmp/creait/Build" --config Release
+sudo cmake --install "/tmp/creait/Build" --prefix "$InstallPath" --config Release
+
+# Build reai-r2
+cmake -S "/tmp/reai-r2" \
+    -B "/tmp/reai-r2/Build" \
+    -D CMAKE_PREFIX_PATH="$InstallPath" \
+    -D CMAKE_INSTALL_PREFIX="$InstallPath" \
+    -D CMAKE_INSTALL_RPATH="$InstallPath/lib" \
+    -D CMAKE_LIBRARY_PATH="$InstallPath/lib" \
+    -D CMAKE_POLICY_VERSION_MINIMUM="3.5"
+cmake --build "/tmp/reai-r2/Build" --config Release
+sudo cmake --install "/tmp/reai-r2/Build" --prefix "$InstallPath" --config Release
+
+sudo chown -R $USER "$(r2 -H R2_USER_PLUGINS)/libreai_radare.dylib" 
---
+++ Scripts/Uninstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+InstallPath="/usr/local"
+echo "Assuming install path $InstallPath"
+
+# Remove installed headers
+sudo rm -rf "$InstallPath/include/Reai"
+sudo rm -rf "$InstallPath/include/cjson"
+sudo rm "$InstallPath/include/toml.h"
+
+# Remove installed libraries
+sudo rm -rf "$InstallPath/lib/libreai*"
+sudo rm -rf "$InstallPath/lib/libcjson*"
+sudo rm -rf "$InstallPath/lib/libtoml*"
+sudo rm -rf "$InstallPath/lib/cmake/cJSON"
+
+# Remove plugin
+sudo rm "$(r2 -H R2_USER_PLUGINS)/libreai_radare.dylib"
