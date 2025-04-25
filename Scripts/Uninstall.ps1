c6061d943b7153fbfb45a55b5dc6c874a7939999
Siddharth Mishra <37365905+brightprogrammer@users.noreply.github.com>
2025-04-25 15:11:50 +0530

    Fix Plugin Path In Uninstall Script

--- Scripts/Uninstall.ps1
+++ Scripts/Uninstall.ps1
@@ -1,3 +1,3 @@
-rm "$(r2 -H R2_USER_PLUGINS)\\libreai_radare.dll"
-rm "$(r2 -H R2_USER_PLUGINS)\\libreai_radare.lib"
+rm "$(r2 -H R2_USER_PLUGINS)\\reai_radare.dll"
+rm "$(r2 -H R2_USER_PLUGINS)\\reai_radare.lib"
 rmdir Build
