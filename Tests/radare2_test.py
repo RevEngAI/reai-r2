71e301ae39f1f70a11dc6da44498204656fb9226
Siddharth Mishra <admin@brightprogrammer.in>
2025-07-13 19:50:25 +0530

    Debug  Commit

--- Tests/radare2_test.py
+++ Tests/radare2_test.py
@@ -11,7 +11,7 @@ def test_root_cmd_desc(r2):
     required_cmds = ['REf', 'REa', 'REc', 'REcs', 'REca', 'REcd', 'RE', 'REb']
 
     for cmd  in required_cmds:
-        print('======================================= Testing for command {cmd} =========================================')
+        print(f'======================================= Testing for command {cmd} =========================================')
         o = r2.cmd(f'{cmd}?')
         print(o)
         if (cmd + ' ') not in out or 'ERROR' in o:
@@ -34,7 +34,7 @@ def test_root_cmd_group_desc(r2):
     required_cmds = ['REa', 'REf', 'REb', 'REc', 'REd', 'REh', 'REi', 'REm', 'REu']
 
     for cmd in required_cmds:
-        print('======================================= Testing for command {cmd} =========================================')
+        print(f'======================================= Testing for command {cmd} =========================================')
         o = r2.cmd(f'{cmd}?')
         print(o)
         if (cmd + ' ') not in out or 'ERROR' in o:
